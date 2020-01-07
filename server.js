2b68e682e5fb2bdbb0098c49f9eaa02e1aba6549
Michelle Quinn <michelle.n.quinn@gmail.com>
2020-01-06 20:46:46 -0600

    heroku connection

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 
-let PORT = process.env.PORT||3000;
+let PORT = process.env.PORT||8080;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
